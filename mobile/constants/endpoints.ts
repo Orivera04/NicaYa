8be00ef2d5734211b30d698966e14884d332998d
Oscar Rivera <oscar.rivera@skydropx.com>
2026-07-11 11:36:26 -0600

    chore: remove all project files

--- mobile/constants/endpoints.ts
+++
@@ -1,4 +0,0 @@
-import Constants from 'expo-constants';
-
-const extra = Constants.expoConfig?.extra as { apiBaseUrl?: string } | undefined;
-export const API_BASE_URL = extra?.apiBaseUrl ?? 'http://localhost:3000/api';
